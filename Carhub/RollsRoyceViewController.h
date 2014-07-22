998656360e33f52ff8d7104b868be08af3f889e2
Christopher Clark <cclark9311@gmail.com>
2014-07-22 00:06:21 -0400

    finished all the damn view controllers

---
+++ Carhub/RollsRoyceViewController.h
@@ -0,0 +1,27 @@
+//
+//  RollsRoyceViewController.h
+//  Carhub
+//
+//  Created by Christopher Clark on 7/21/14.
+//  Copyright (c) 2014 Ham Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Model.h"
+#import "Make.h"
+
+@interface RollsRoyceViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray * jsonArray;
+@property (nonatomic, strong) NSMutableArray * carArray;
+@property (nonatomic, retain) NSArray * modelArray;
+
+@property (nonatomic, strong) Make * currentMake;
+
+
+
+#pragma mark-
+#pragma mark Class Methods
+- (void) retrieveData;
+
+@end
