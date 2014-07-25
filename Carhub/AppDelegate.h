e89bbb087897c3dd3d203c588d075f30646c7308
Christopher Clark <cclark9311@gmail.com>
2014-07-25 00:26:25 -0400

    fixed

--- Carhub/AppDelegate.h
+++ Carhub/AppDelegate.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ADBannerView *UIiAD;
 
 @end
